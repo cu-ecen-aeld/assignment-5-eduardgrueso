1be8b45b8373dc22d11b4514b53e8a92cf98f265
eduard <egrueso@unal.edu.co>
2026-02-23 16:02:45 -0500

    assignment-9-complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '02dbfbc64324f7f92b84b2b6a97859692382f8dd'
+AESD_ASSIGNMENTS_VERSION = 'ad3bc5533bc387e2cf352bfcfeddd10b856ce7ba'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-eduardgrueso.git'
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignment-9-eduardgrueso.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
--- base_external/package/aesd-char-driver/aesd-char-driver.mk
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -5,11 +5,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHAR_DRIVER_VERSION = '02dbfbc64324f7f92b84b2b6a97859692382f8dd'
+AESD_CHAR_DRIVER_VERSION = 'ad3bc5533bc387e2cf352bfcfeddd10b856ce7ba'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_CHAR_DRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignment-8-eduardgrueso.git'
+AESD_CHAR_DRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignment-9-eduardgrueso.git'
 AESD_CHAR_DRIVER_SITE_METHOD = git
 AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
 AESD_CHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver/
